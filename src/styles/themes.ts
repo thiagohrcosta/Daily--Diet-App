7d32022ddcdf8d9fd96cd1f4954586be4ee51f20
Thiagohrcosta <thiagohrcosta86@gmail.com>
2023-04-02 21:12:11 -0300

    Project created with base setup

---
+++ src/styles/themes.ts
@@ -0,0 +1,27 @@
+import { extendTheme } from '@chakra-ui/react';
+
+export const theme = extendTheme({
+  colors: {
+
+    principal: "#DEDEDE",
+    
+    redDark: '#BF3B44',
+    redMid: '#F3BABD',
+    redlight: '#F4E6E7',
+
+    greenDark: '#639339',
+    greenMid: '#CBE4B4',
+    greenLight: '#E5F0DB',
+
+    gray100: '#1B1D1E',
+    gray200: '#333638',
+    gray300: '#5C6265',
+    gray400: '#B9BBBC',
+    gray500: '#DDDEDF',
+    gray600: '#DDDEDF',
+    gray700: '#FAFAFA',
+    
+    white: '#FFFFFF',
+
+  }
+});
